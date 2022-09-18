721fd1d118c613ada116ed4554a1d991cdacb7a9
NGUYEN KHANH <khanh.nguyen3@monstarlab.com>
2022-09-18 21:20:31 +0700

    fix-bug

--- src/components/Modules/ImageItem/index.js
+++ src/components/Modules/ImageItem/index.js
@@ -1,3 +1,4 @@
+import ProgressBar from "../ProgressBar";
 import trash from "./trash.svg";
 
 const ImageItem = (props) => {
@@ -6,12 +7,15 @@ const ImageItem = (props) => {
   return (
     <li className="image-item">
       <div className="image-content">
-        <img
-          className="upload-image"
-          src={URL.createObjectURL(item.data)}
-          alt={item.data.name}
-          onClick={() => handleShowImage(item.data)}
-        />
+        <div className="image-progress">
+          <img
+            className="upload-image"
+            src={URL.createObjectURL(item.data)}
+            alt={item.data.name}
+            onClick={() => handleShowImage(item.data)}
+          />
+          <ProgressBar />
+        </div>
         <h5 className="image-name">
           {item.data.name.length > 35
             ? item.data.name?.substring(0, 10) +
---
+++ src/components/Modules/ProgressBar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProgressBar = () => {
+  return (
+    <div className="myProgress">
+      <div className="myBar"></div>
+    </div>
+  );
+};
+
+export default ProgressBar;
